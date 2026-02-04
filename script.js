4e33cabe3ab533515b1fa43b8945a2a3d5624fa9
luis <luis.miguelfsdeus@gmail.com>
2026-02-04 16:12:05 +0000

    feat: add HTML structure and base CSS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-script.js
 console.log('🚀 Portfolio carregado com sucesso!');
 
 // Teste: mudar cor de fundo ao clicar
 document.body.addEventListener('click', () => {
     console.log('Clicaste na página!');
 });
+
+getCurrentYear()
+updateFooterYear()
+toggleTheme()
