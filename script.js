031f2f59a69246aae61e13cda80ad2ed266a6951
luis <luis.miguelfsdeus@gmail.com>
2026-02-04 15:15:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+script.js
+console.log('🚀 Portfolio carregado com sucesso!');
+
+// Teste: mudar cor de fundo ao clicar
+document.body.addEventListener('click', () => {
+    console.log('Clicaste na página!');
+});
